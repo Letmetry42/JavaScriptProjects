1412d2a5011939c6dc29035d342ebbb2a9523e7f
Letmetry42 <connectwithnasir42@gmail.com>
2020-08-24 01:42:07 +0500

    partially comp project 003

--- P-003/script.js
+++ P-003/script.js
@@ -1,54 +1,83 @@
-const video = document.getElementById('video');
+const video = document.querySelector('#video');
 const play = document.getElementById('play');
 const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
-
-//functons
-// 1- togglevideo - play or pause video
+// Functions
+// 1 - toggleVideo - Play or Pause video
 // If video is playing, then pause
 // If video is paused, then play
-function toogleVideo() {
-    return true;
-}
+function toggleVideo() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+};
 
-// 2- update
+// 2 - updateIcon - toggle between play and pause icons
+// If video is playing, then show pause icon
+// If video is paused, then show play icon
+function updateIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fas fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class="fas fa-pause fa-2x"></i>';
+    }
+};
 
-
-
-
-// 3 - updateProgress - update the position of the progress bar 
+// 3 - updateProgress - update the position of the progress bar and timestamp
 function updateProgress() {
-    return true;
-}
+    // Update slider
+    progress.value = video.currentTime/video.duration*100;
 
+    // Update timestamp
+    // Rounding down the minutes
+    let minutes = Math.floor(video.currentTime / 60);
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
 
-// 4 - StopVideo - stop video playback and reset time to 0
-function 
+    // Rounding down the seconds
+    let seconds = Math.floor(video.currentTime % 60);
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
+    
+    // Display Timestamp
+    timestamp.innerHTML = `${minutes}:${seconds}`;
+};
 
+// 4 - stopVideo - Stop video playback and reset time to 0
+function stopVideo() {
+    video.pause();
+    video.currentTime = 0;
+};
 
-// 5 - setProgress - update video playback time based on manual
+// 5 - setProgress - update video playback time based on manual change in progress bar
 function setProgress() {
-    return true;
-}
-// Event Listeners
-// 1- video Element - click to play or pause video
-video.addEventListener('click', toogleVideo);
+    video.currentTime = progress.value * video.duration / 100;
+};
 
-// 2- Video Element - pause to toggle play icon to pause icon
+// Event Listeners
+// 1 - Video Element - click to play or pause video
+video.addEventListener('click', toggleVideo);
+
+// 2 - Video Element - pause to toggle play icon to pause icon
 video.addEventListener('pause', updateIcon);
 
-// 3- video Elemnet - play to toogle pause icon back to play icon
+// 3 - Video Element - play to toggle pause icon back to play icon
 video.addEventListener('play', updateIcon);
 
-// 4- video Element - update progress bar and timestamp
-video.addEventListener('timeupdagte', updateProgress);
+// 4 - Video Element - update progress bar and timestamp
+video.addEventListener('timeupdate', updateProgress);
 
-// 5- play button - click to play or pause video
+// 5 - Play Button - click to play or pause video
 play.addEventListener('click', toggleVideo);
 
-// 6- stop button - click to reset video and pause video
-Stop.addEventListener('click', stopVideo);
+// 6 - Stop Button - click to reset video and pause video
+stop.addEventListener('click', stopVideo);
 
-// 7- Progress Bar - change position to change time to playback
+// 7 - Progress Bar - change position to change time of playback
+progress.addEventListener('change', setProgress);
