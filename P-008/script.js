c92efe119aed026337b55ec1a8064a9cdc40ebb7
Letmetry42 <connectwithnasir42@gmail.com>
2020-10-06 13:21:47 +0500

    progress on P-008

--- P-008/script.js
+++ P-008/script.js
@@ -12,6 +12,7 @@ function searchMeal(e) {
 
     // Get the search term from input field
     const term = search.value;
+    console.log(term);
     
 
     // Check if search term exists
@@ -19,23 +20,56 @@ function searchMeal(e) {
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
             .then(res => res.json())
             .then(data => {
-                console.log(data);
+                resultHeading.innerHTML = `<h2>Search results for "${term}"</h2>`
+                if(data.meals == null){
+                    resultHeading.innerHTML = `<p>There are no search results for "${term}". Please enter a valid keyword. </P>`
+                } else {
+                    mealContainer.innerHTML = data.meals.map( meal => `
+                        <div class="meal">
+                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+                            <div class="meal-info" data-mealID="${meal.idMeal}">
+                                <h3>${meal.strMeal}</h3>
+                            </div>
+                        </div>
+                    `)
+                    .join('')
+                }
+
 
             })
 
     } else  {
         alert('Please enter a valid search')
 
-    }
+    } 
 
-
+    // Clear Search Term
+    search.value= '';
 }
 
 
 
 // Event listeners
-// 1: Submit
-submit.addEventListener('submit', searchMeal)
+// 1: Submit Form
+submit.addEventListener('submit', searchMeal);
+
+// 2: When clicking a Meal
+mealContainer.addEventListener('click', e => {
+    const mealInfo = e.path.find ( item => {
+        console.log(item);
+        if(item.classList) {
+            return item.classList.contains('meal-Info');
+        } else {
+                return false
+        }
+        
+    });
+
+    if(mealInfo) {
+        const mealID = mealInfo.getAttribute('data-mealid');
+        console.log(mealID);
+    }   // at 1:24:01
+});
 
 
 
