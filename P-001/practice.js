6a92c060aa41a05171253e3cc464aa62cabe2ac3
Letmetry42 <connectwithnasir42@gmail.com>
2021-01-14 17:27:42 +0500

    progress

---
+++ P-001/practice.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+
+// All functions
+// Function to show error
+function showError(input, message) {}
+
+// This is an event listner for the form on submit
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if (username.nodeValue === '') {
+
+    }
+})
